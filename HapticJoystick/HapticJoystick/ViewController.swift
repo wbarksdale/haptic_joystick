41a68f72a3c73c8c1615c0c77c97f74291a03e36
= <=>
2018-07-20 09:28:09 -0700

    initial commit

---
+++ HapticJoystick/HapticJoystick/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HapticJoystick
+//
+//  Created by William Barksdale on 7/20/18.
+//  Copyright © 2018 William Barksdale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
